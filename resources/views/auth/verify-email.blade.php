4515bd8ac1bb2a7d93daed0e92af4a26d432cc5f
David Smith <=>
2025-05-13 14:55:11 -0700

    dd user

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,12 +1,13 @@
 <x-guest-layout>
-    <?php
-    $user = auth()->user();
-    ?>
+   
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
-
+ <?php
+    $user = auth()->user();
+    dd($user);
+    ?>
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
         </div>
