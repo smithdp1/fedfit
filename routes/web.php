4ef4d53263b9b79756beacf0fca9d6e800868935
David Smith <=>
2025-05-13 09:45:03 -0700

    fixing middleware

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,15 @@ Route::get('/profile/{user:username}', Profile::class)->name('profile.show');
 
 // I want to make sure email verification is required for all routes
 // except for the home page and the profile page using the ensureEmailIsVerified middleware
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/tweet/{tweet:uuid}', Tweet::class)->name('tweet.show');
+Route::middleware(['auth:sanctum',
+config('jetstream.auth_session'),
+'verified'])->group(function () {
+   Route::get('/tweet/{tweet:uuid}', Tweet::class)->name('tweet.show');
     Route::get('/notifications', Notifications::class)->name('notifications');
     Route::get('/follower/list', FollowerList::class)->name('follower.list');
 });
+// Route::middleware('auth')->group(function () {
+//     Route::get('/tweet/{tweet:uuid}', Tweet::class)->name('tweet.show');
+//     Route::get('/notifications', Notifications::class)->name('notifications');
+//     Route::get('/follower/list', FollowerList::class)->name('follower.list');
+// });
